15054fb3baf4c9ac41bdcbda034c879f668c62d7
Eyen <eyentuyon@gmail.com>
2022-05-19 21:21:35 -0300

    Add confirm password function

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene);
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#confirmPassword');
+
+function confirm() {
+    if (password.value !== password2.value) {
+        password.classList.add('error');
+        password2.classList.add('error');
+    } else {
+        password.classList.remove('error')
+        password2.classList.remove('error')
+    }
+}
+
+password.addEventListener('change', confirm)
+password2.addEventListener('change', confirm)
+
+let scene = document.getElementById('scene');
+let parallaxInstance = new Parallax(scene);
